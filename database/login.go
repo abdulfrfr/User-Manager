ac6f20c88fd6d5ec709e165d32d4e8f219b78461
abdulfrfr <abdulmails112@gmail.com>
2024-11-18 11:19:27 +0100

    fixed /login endpoint

--- database/login.go
+++ database/login.go
@@ -7,16 +7,18 @@ import (
 
 func Login(res http.ResponseWriter, username string) (interface{}, error) {
 	var user Users
-	rows, err := connected.DB.Query("SELECT * FROM your_table WHERE name = $1", username)
+	rows, err := connected.DB.Query("SELECT * FROM list WHERE name = $1", username)
 
 	if err != nil {
 		return nil, err
 	}
 
-	err = rows.Scan(&user.ID, &user.Name)
+	for rows.Next() {
+		err = rows.Scan(&user.ID, &user.Name)
 
-	if err != nil {
-		log.Fatal(err)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	return user.Name, nil
